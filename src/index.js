aac1c723636777bd9761c1c5342a7439d84f6299
Ozzeek <osama.m.alnagar@gmail.com>
2023-10-30 21:33:04 +0200

    moved library related code to module

--- src/index.js
+++ src/index.js
@@ -8,27 +8,35 @@ const tableModule = (function () {
       // create a new row in the table with the td of the object
       const newRow = document.createElement(`tr`);
       newRow.setAttribute(`data-attribute`, `${index}`);
-      newRow.innerHTML = `<td>${element.title}</td><td>${element.author}</td><td>${element.pages}</td><td><button class="p-1 border border-gray-200 rounded-lg my-auto" onclick="toggleRead(event)">${element.read}</button></td><td><button class="p-1 border border-gray-200 rounded-lg my-auto " id="deleteBtn" onclick="tableModule.deleteRow(event)">Delete</button></td>`;
+      newRow.innerHTML = `<td>${element.title}</td><td>${element.author}</td><td>${element.pages}</td><td><button class="p-1 border border-gray-200 rounded-lg my-auto" onclick="tableModule.toggleRead(event)">${element.read}</button></td><td><button class="p-1 border border-gray-200 rounded-lg my-auto " id="deleteBtn" onclick="tableModule.deleteRow(event)">Delete</button></td>`;
       document.querySelector("tbody").appendChild(newRow);
     });
-
-    function deleteRow(event) {
-      // get parent element data-attribute
-      const index =
-        event.target.parentElement.parentElement.getAttribute("data-attribute");
-      // remove it from the array
-      myLibrary.splice(index, 1);
-
-      // redraw the table
-      addToTable(myLibrary);
-    }
-
-    return {
-      myLibrary: myLibrary,
-      addToTable: addToTable,
-      deleteRow: deleteRow,
-    };
   }
+  function deleteRow(event) {
+    // get parent element data-attribute
+    const index =
+      event.target.parentElement.parentElement.getAttribute("data-attribute");
+    // remove it from the array
+    myLibrary.splice(index, 1);
+
+    // redraw the table
+    addToTable(myLibrary);
+  }
+
+  function toggleRead(event) {
+    const index =
+      event.target.parentElement.parentElement.getAttribute("data-attribute");
+    // myLibrary[index].setRead();
+    myLibrary[index].setRead();
+    addToTable(myLibrary);
+  }
+
+  return {
+    myLibrary: myLibrary,
+    addToTable: addToTable,
+    deleteRow: deleteRow,
+    toggleRead: toggleRead,
+  };
 })();
 
 // dialog module
@@ -70,7 +78,7 @@ const dialogModule = (function () {
     );
 
     tableModule.myLibrary.push(newBook);
-    tableModule.addToTable(myLibrary);
+    tableModule.addToTable(tableModule.myLibrary);
     // reset form elements and close dialog
     myForm.reset();
     insDialog.close();
